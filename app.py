d2f455f04adb237b79601af525ae14f956de921f
wissem007 <alouiwiss@gmail.com>
2023-08-07 11:20:22 +0100

    modification python

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Welcome to Mr DevOps Youtube Channel"
+    return "Welcome to  DevOps wissem Test Pipeline"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
