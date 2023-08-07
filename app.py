73243cee491be8be00ae6b5e8a4916e125921e21
wissem007 <alouiwiss@gmail.com>
2023-08-07 14:33:41 +0100

    modification python2

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Welcome to  DevOps wissem Test Pipeline avec gitub"
+    return "Deployment @Devops@ TALYS pipeline  jenkins: DevOps Tools Engineer"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
