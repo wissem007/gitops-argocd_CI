d473da71dc1424b8288a1d7a0572094859ebce06
wissem007 <alouiwiss@gmail.com>
2023-08-08 13:01:31 +0100

    modification python6

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ def health():
 
 @app.route("/")
 def home():
-    return "Deployment @Devops@ pipeline 5 jenkins: DevOps Tools Engineer"
+    return "Deployment @Devops@ pipeline 6 jenkins: DevOps Tools Engineer"
    
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
